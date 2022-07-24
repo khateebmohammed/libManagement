fa5c6667ed4543c74294b3e44e5a6206f3e3412c
Abdulazizmuthanna <abdualzizmuhmmd@gmail.com>
2022-07-25 00:21:37 +0300

    I have do all process in login and sign up pages

--- libManagement/mainPage/create_account.Designer.cs
+++ libManagement/mainPage/create_account.Designer.cs
@@ -31,11 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(create_account));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txt_User_ID = new System.Windows.Forms.TextBox();
+            this.txt_User_PWD = new System.Windows.Forms.TextBox();
+            this.btn_Create_User = new System.Windows.Forms.Button();
+            this.txt_Conf_User_PWD = new System.Windows.Forms.TextBox();
+            this.cmb_User_Type = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -51,7 +51,7 @@
             this.label1.Location = new System.Drawing.Point(139, 43);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 29);
+            this.label1.Size = new System.Drawing.Size(94, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "انشاء حساب";
             // 
@@ -59,7 +59,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(20, 21);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(32, 32);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -67,69 +67,74 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
             // 
-            // textBox1
+            // txt_User_ID
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(71, 255);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBox1.Size = new System.Drawing.Size(252, 34);
-            this.textBox1.TabIndex = 3;
+            this.txt_User_ID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_User_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_User_ID.Location = new System.Drawing.Point(71, 255);
+            this.txt_User_ID.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_User_ID.Multiline = true;
+            this.txt_User_ID.Name = "txt_User_ID";
+            this.txt_User_ID.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_User_ID.Size = new System.Drawing.Size(252, 31);
+            this.txt_User_ID.TabIndex = 3;
             // 
-            // textBox2
+            // txt_User_PWD
             // 
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(71, 348);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBox2.Size = new System.Drawing.Size(252, 34);
-            this.textBox2.TabIndex = 4;
+            this.txt_User_PWD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_User_PWD.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_User_PWD.Location = new System.Drawing.Point(71, 348);
+            this.txt_User_PWD.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_User_PWD.Multiline = true;
+            this.txt_User_PWD.Name = "txt_User_PWD";
+            this.txt_User_PWD.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_User_PWD.Size = new System.Drawing.Size(252, 31);
+            this.txt_User_PWD.TabIndex = 4;
             // 
-            // button1
+            // btn_Create_User
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(157)))), ((int)(((byte)(219)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(157)))), ((int)(((byte)(219)))));
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(117)))), ((int)(((byte)(205)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
-            this.button1.Location = new System.Drawing.Point(96, 560);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 49);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "إنشاء";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.btn_Create_User.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(157)))), ((int)(((byte)(219)))));
+            this.btn_Create_User.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(157)))), ((int)(((byte)(219)))));
+            this.btn_Create_User.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(117)))), ((int)(((byte)(205)))));
+            this.btn_Create_User.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(87)))), ((int)(((byte)(194)))));
+            this.btn_Create_User.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Create_User.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Create_User.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_Create_User.Location = new System.Drawing.Point(96, 560);
+            this.btn_Create_User.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Create_User.Name = "btn_Create_User";
+            this.btn_Create_User.Size = new System.Drawing.Size(205, 49);
+            this.btn_Create_User.TabIndex = 6;
+            this.btn_Create_User.Text = "إنشاء";
+            this.btn_Create_User.UseVisualStyleBackColor = false;
+            this.btn_Create_User.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // textBox3
+            // txt_Conf_User_PWD
             // 
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(71, 441);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBox3.Size = new System.Drawing.Size(252, 34);
-            this.textBox3.TabIndex = 5;
+            this.txt_Conf_User_PWD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Conf_User_PWD.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Conf_User_PWD.Location = new System.Drawing.Point(71, 441);
+            this.txt_Conf_User_PWD.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Conf_User_PWD.Multiline = true;
+            this.txt_Conf_User_PWD.Name = "txt_Conf_User_PWD";
+            this.txt_Conf_User_PWD.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_Conf_User_PWD.Size = new System.Drawing.Size(252, 34);
+            this.txt_Conf_User_PWD.TabIndex = 5;
             // 
-            // comboBox1
+            // cmb_User_Type
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(71, 170);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(254, 24);
-            this.comboBox1.TabIndex = 7;
+            this.cmb_User_Type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_User_Type.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmb_User_Type.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_User_Type.FormattingEnabled = true;
+            this.cmb_User_Type.Items.AddRange(new object[] {
+            "مدير",
+            "مستخدم عادي"});
+            this.cmb_User_Type.Location = new System.Drawing.Point(71, 170);
+            this.cmb_User_Type.Name = "cmb_User_Type";
+            this.cmb_User_Type.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cmb_User_Type.Size = new System.Drawing.Size(254, 26);
+            this.cmb_User_Type.TabIndex = 7;
             // 
             // label3
             // 
@@ -138,7 +143,7 @@
             this.label3.ForeColor = System.Drawing.Color.SlateBlue;
             this.label3.Location = new System.Drawing.Point(240, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.Size = new System.Drawing.Size(69, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "نوع المستخدم";
             // 
@@ -149,7 +154,7 @@
             this.label4.ForeColor = System.Drawing.Color.SlateBlue;
             this.label4.Location = new System.Drawing.Point(241, 231);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.Size = new System.Drawing.Size(68, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "اسم المستخدم";
             // 
@@ -160,7 +165,7 @@
             this.label5.ForeColor = System.Drawing.Color.SlateBlue;
             this.label5.Location = new System.Drawing.Point(247, 324);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.Size = new System.Drawing.Size(62, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "كلمة المرور";
             // 
@@ -171,13 +176,13 @@
             this.label6.ForeColor = System.Drawing.Color.SlateBlue;
             this.label6.Location = new System.Drawing.Point(217, 417);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(106, 20);
+            this.label6.Size = new System.Drawing.Size(86, 17);
             this.label6.TabIndex = 11;
             this.label6.Text = "تأكيد كلمة المرور";
             // 
             // create_account
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
             this.ClientSize = new System.Drawing.Size(397, 652);
@@ -185,16 +190,16 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmb_User_Type);
+            this.Controls.Add(this.btn_Create_User);
+            this.Controls.Add(this.txt_Conf_User_PWD);
+            this.Controls.Add(this.txt_User_PWD);
+            this.Controls.Add(this.txt_User_ID);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "create_account";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "create_account";
@@ -209,11 +214,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txt_User_ID;
+        private System.Windows.Forms.TextBox txt_User_PWD;
+        private System.Windows.Forms.Button btn_Create_User;
+        private System.Windows.Forms.TextBox txt_Conf_User_PWD;
+        private System.Windows.Forms.ComboBox cmb_User_Type;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
--- libManagement/mainPage/create_account.cs
+++ libManagement/mainPage/create_account.cs
@@ -13,7 +13,7 @@ namespace libManagement.mainPage
 {
     public partial class create_account : Form
     {
-
+        Business_Layer.LOGIN_CLASS Log_Class = new Business_Layer.LOGIN_CLASS();
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
        (
@@ -27,17 +27,47 @@ namespace libManagement.mainPage
         public create_account()
         {
             InitializeComponent();
+            cmb_User_Type.SelectedIndex = 1;
         }
 
         private void Create_account_Load(object sender, EventArgs e)
         {
-            button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 30, 30));
+            btn_Create_User.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btn_Create_User.Width, btn_Create_User.Height, 30, 30));
 
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (txt_User_PWD.Text == txt_Conf_User_PWD.Text)
+                {
+                    if (txt_User_ID.Text != "" && txt_User_PWD.Text != "")
+                    {
+                        Log_Class.ADD_System_User(txt_User_ID.Text, txt_User_PWD.Text, cmb_User_Type.SelectedItem.ToString());
+                        MessageBox.Show("تم إضافة حساب جديد بنجاح", "تأكيد إضافة الحساب", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        txt_User_ID.Clear();
+                        txt_User_PWD.Clear();
+                        txt_Conf_User_PWD.Clear();
+                        cmb_User_Type.SelectedIndex = 1;
+                        txt_User_ID.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("يجب تعبئة جميع الحقول", "خطأ في إنشاء حساب جديد", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txt_User_ID.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("كلمات المرور غير متطابقة!", "خطأ في كلمة المرور", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_Conf_User_PWD.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("حدث خطأ ما", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void PictureBox1_Click(object sender, EventArgs e)
